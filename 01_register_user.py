dd065ff2be7501fc58844fdbe4d1a0af8deaf526
Luis. <luis.henrique.18@outlook.com>
2024-09-10 22:09:12 -0300

    Update 01_register_user.py

--- 01_register_user.py
+++ 01_register_user.py
@@ -17,3 +17,30 @@ while true:
     if actions == 'to enter':
         if values['user'] == 'name' and values['password'] == '123456':
                 print('Welcome')
+
+#OR_THE_NEXT_CODE
+
+import PySimpleGUI as sg
+
+# layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('User'), sg.Input(key='user', size=(20, 1))],
+    [sg.Text('Password'), sg.Input(key='password', password_char='*', size=(20, 1))],
+    [sg.Checkbox('Save login?')],
+    [sg.Button('To Enter')]
+]
+
+# window
+window = sg.Window('Login Screen', layout)
+
+# action
+while True:
+    events, values = window.read()
+    if events == sg.WIN_CLOSED:
+        break
+    if events == 'To Enter':
+        if values['user'] == 'name' and values['password'] == '123456':
+            print('Welcome')
+
+window.close()
