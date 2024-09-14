61aebd21958c5b3a6219e8c1373222f159b68733
Luis. <luis.henrique.18@outlook.com>
2024-09-14 11:40:46 -0300

    Update 04_numerical_guessing_ game.py

--- 04_numerical_guessing_ game.py
+++ 04_numerical_guessing_ game.py
@@ -4,7 +4,7 @@ print("Welcome to the guessing game!")
 choice_number = input("Enter the ceiling number of the challenge: ")
 
 if choice_number.isdigit():
-    choice_number = (choice_number)
+    choice_number = int(choice_number)  
 else:
     print("Error: informed value is not numerical. Please run again and enter a number!")
     quit()
@@ -13,7 +13,7 @@ random_number = random.randint(0, choice_number)
 
 n_choices = 0
 
-while true:
+while True:  
     answer_user = input("Guess the number: ")
 
     if answer_user.isdigit():
@@ -22,13 +22,14 @@ while true:
         print("Error: informed value is not numerical. Please enter a number!")
         continue
 
-n_choices = n_choices + 1
-if answer_user == random_number:
-    print("You got it right!")
-    break
-elif answer_user > random_number:
-    print("Guessed high, the number is smaller...")
-else:
-    print("Guess low, the number is bigger...")
+    n_choices += 1  
 
-    print("number of attempts: " + str(n_choices))
+    if answer_user == random_number:
+        print("You got it right!")
+        break
+    elif answer_user > random_number:
+        print("Guessed high, the number is smaller...")
+    else:
+        print("Guess low, the number is bigger...")
+
+print("Number of attempts: " + str(n_choices))  
