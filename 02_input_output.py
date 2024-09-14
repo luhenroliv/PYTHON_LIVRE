fb806dd029b342877f3886e9356597effa119aeb
Luis. <luis.henrique.18@outlook.com>
2024-09-14 11:07:28 -0300

    Update 02_input_output.py

--- 02_input_output.py
+++ 02_input_output.py
@@ -1,4 +1,4 @@
-#Version1
+#VERSION1
 print("What's your name?")
 name = input()
 print('Welcome %s' % name  )
@@ -6,7 +6,7 @@ print("How old are you?")
 old = input()
 print('%s is %s years old' % (name, old   ))
 
-#Version2
+#VERSION2
 print("What's your name?")
 name = input()
 print('Welcome {0}'.format(name))
@@ -14,17 +14,26 @@ print("How old are you?")
 old = input()
 print('{0} is {1} years old'.format(name, old))
 
-#Version3
+#VERSION3
 print("What's your name?")
 name = input()
 print(f'Welcome {name} ')
 print("How old are you?")
 old = input()
 print(f'{name} is {old} years old')
-print(f'{name} born in {2023 - int(old)}')
+print(f'{name} born in {2024 - int(old)}')
 
-#Version4
-name = input('What's your name ')
+#VERSION4
+# Solicita o nome do usuário
+name = input("What's your name? ")
 print(f'Welcome {name}')
-old = input('How old are you? ')
-print(f'{name} born in {2023 - int(old)}')
+
+# Solicita a idade do usuário
+old = input("How old are you? ")
+try:
+    # Calcula o ano de nascimento e exibe a mensagem
+    year_of_birth = 2024 - int(old)
+    print(f'{name} was born in {year_of_birth}')
+except ValueError:
+    # Trata o caso em que a idade fornecida não é um número
+    print("Please enter a valid number for age.")
